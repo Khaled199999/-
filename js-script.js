61e11a73eb33ce3c0fc5123ef501d4c7a48b9fec
Khaled199999 <kldloub@yahoo.com>
2025-06-28 16:56:37 +0300

    Add files via upload

---
+++ js-script.js
@@ -0,0 +1,8 @@
+function playSound() {
+  const audio = new Audio('audio/alef.mp3');
+  audio.play();
+}
+
+function showCertificate() {
+  document.getElementById('certificate').style.display = 'block';
+}
